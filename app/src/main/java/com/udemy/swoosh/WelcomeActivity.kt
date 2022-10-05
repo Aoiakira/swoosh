1d45de398d4f0d977fb8902fe55e45b10a69ca4f
Arushi Jain <arushiejain@gmail.com>
2022-10-05 21:45:12 +0530

    Swoosh 2.1

--- app/src/main/java/com/udemy/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/udemy/swoosh/WelcomeActivity.kt
@@ -1,11 +1,19 @@
 package com.udemy.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val btn = findViewById(R.id.getStartedBtn) as Button
+        btn.setOnClickListener {
+            val leagueIntent = Intent (this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
